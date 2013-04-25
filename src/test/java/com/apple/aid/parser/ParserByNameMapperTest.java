63811ced9969d048ac9de0b6ba89de95cfb33de9
Georgi Lambov <g.lambov@zariba.com>
2013-04-25 11:14:15 +0300

    Imports orgaization.

--- src/test/java/com/apple/aid/parser/ParserByNameMapperTest.java
+++ src/test/java/com/apple/aid/parser/ParserByNameMapperTest.java
@@ -4,7 +4,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
